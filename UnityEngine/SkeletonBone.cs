6641a565e03da6909c7f55e50cbe79e8fb1d8751
dmineau <dmineau@gmail.com>
2019-07-23 19:22:15 -0400

    I think I'm done adding UMA files. Just need to get the ones I have compiling(282 errors)

--- UnityEngine/SkeletonBone.cs
+++ UnityEngine/SkeletonBone.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Godot;
 
 namespace UnityEngine
 {
     public class SkeletonBone
     {
+        public string name
+        {
+            get; set;
+        }
+
+        public Vector3 position
+        {
+            get; set;
+        }
+
+        public Quaternion rotation
+        {
+            get; set;
+        }
+
+        public Vector3 scale
+        {
+            get; set;
+        }
     }
 }
